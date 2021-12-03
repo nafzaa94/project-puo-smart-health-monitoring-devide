88864463a7081f0a1337a36931c9302c2a093ed0
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2021-12-04 03:04:53 +0800

    Update wifi_test.ino

--- wifi_test.ino
+++ wifi_test.ino
@@ -1,14 +1,19 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
 //Your Domain name with URL path or IP address with path
-String serverName = "http://192.168.1.106";
+String serverName = "http://192.168.1.106:1880/update-sensor";
 
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
-
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
 unsigned long timerDelay = 5000;
 
 void setup() {
@@ -28,16 +33,17 @@ void setup() {
 }
 
 void loop() {
-  //Send an HTTP POST request every 10 minutes
+  // Send an HTTP POST request depending on timerDelay
   if ((millis() - lastTime) > timerDelay) {
     //Check WiFi connection status
     if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
       HTTPClient http;
 
-      String serverPath = serverName + "/projectpoli/confirmcard.php?NoGiliran=671163113";
+      String serverPath = serverName + "?temperature=24.37";
       
       // Your Domain name with URL path or IP address with path
-      http.begin(serverPath.c_str());
+      http.begin(client, serverPath.c_str());
       
       // Send HTTP GET request
       int httpResponseCode = http.GET();
