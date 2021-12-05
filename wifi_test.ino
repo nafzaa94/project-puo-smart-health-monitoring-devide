cfb1395e4023ee89d499a72c14e150341a81a138
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2021-12-05 16:06:51 +0800

    Update wifi_test.ino

--- wifi_test.ino
+++ wifi_test.ino
@@ -1,23 +1,64 @@
+// FOR NODEMCU
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <ArduinoJson.h>
 
-const char* ssid = "ptriqstna";
-const char* password = "ptriqstna";
+//spo2 and heartrate
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
 
-//Your Domain name with URL path or IP address with path
-String serverName = "http://172.100.11.143";
+MAX30105 particleSensor;
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
-// Set timer to 5 seconds (5000)
-unsigned long timerDelay = 5000;
+#define MAX_BRIGHTNESS 255
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+String tag = "";
+
+int pin1 = D5;
+int pin2 = D6;
+int pin3 = D8;
+
+int datapin1 = 0;
+int datapin2 = 0;
+int datapin3 = 0;
+
+const char* ssid = "Ptriqstna"; 
+const char* password = "puteriqistina";
+
+String datanocard = "";
+String DataHeartRate = "";
+String DataSPO2 = "";
+
+String serverName = "http://172.20.10.3";
+
+String payload;
+
+StaticJsonDocument<200> doc;
+
 
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(9600);
+  pinMode (datapin1, INPUT);
+  pinMode (datapin2, INPUT);
+  pinMode (datapin3, INPUT);
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -30,17 +71,71 @@ void setup() {
   Serial.println(WiFi.localIP());
  
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
+  }
+
+  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 }
 
 void loop() {
-  // Send an HTTP POST request depending on timerDelay
-  if ((millis() - lastTime) > timerDelay) {
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
+  datapin1 = digitalRead(pin1);
+  datapin2 = digitalRead(pin2);
+  datapin3 = digitalRead(pin3);
+
+  if (datapin1 == HIGH){
+    tag = "671163113";
+    getreqnocard();
+    if (tag == datanocard){
+      scanmax30100();
+      delay(2000);
+      getsenddata(datanocard, DataHeartRate, DataSPO2);
+      tag = "";
+      }
+    }
+
+  if (datapin2 == HIGH){
+    tag = "1911715722";
+    getreqnocard();
+    if (tag == datanocard){
+      scanmax30100();
+      delay(2000);
+      getsenddata(datanocard, DataHeartRate, DataSPO2);
+      tag = "";
+      }
+    }
+
+  if (datapin3 == HIGH){
+    tag = "21115314922";
+    getreqnocard();
+    if (tag == datanocard){
+      scanmax30100();
+      delay(2000);
+      getsenddata(datanocard, DataHeartRate, DataSPO2);
+      tag = "";
+      }
+    }
+}
+
+void getreqnocard(){
+  
+  if(WiFi.status()== WL_CONNECTED){
       WiFiClient client;
       HTTPClient http;
 
-      String serverPath = serverName + "/projectpoli/confirmcard.php?NoGiliran=671163113";
+      String serverPath = serverName + "/api/reqdata/" + tag;
       
       // Your Domain name with URL path or IP address with path
       http.begin(client, serverPath.c_str());
@@ -51,7 +146,7 @@ void loop() {
       if (httpResponseCode>0) {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
-        String payload = http.getString();
+        payload = http.getString();
         Serial.println(payload);
       }
       else {
@@ -64,6 +159,91 @@ void loop() {
     else {
       Serial.println("WiFi Disconnected");
     }
-    lastTime = millis();
+    DeserializationError error = deserializeJson(doc, payload);
+
+    const char* Status = doc["password"];
+    datanocard = String(Status);
   }
-}
+
+void getsenddata(String dataNC , String dataHR, String dataSPO2){   
+  if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+
+      String serverPath = serverName + "/api/senddata/" + dataHR + "/" + dataSPO2 + "/" + dataNC;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverPath.c_str());
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  
+  }  
+
+
+void scanmax30100(){
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.println(irBuffer[i], DEC); 
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  for (byte i = 0 ; i < 20 ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[i], DEC);
+
+      Serial.print(F(", HR="));
+      Serial.print(heartRate, DEC);
+      DataHeartRate = String(heartRate, DEC);
+
+      Serial.print(F(", SPO2="));
+      Serial.println(spo2, DEC);
+      DataSPO2 = String(spo2, DEC); 
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  
+  }
